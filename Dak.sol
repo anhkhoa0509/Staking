1423b2d19a6429a1db6bb9015e9ea7df006b5e6f
Nguyen Le Anh Khoa <78685355+anhkhoa0509@users.noreply.github.com>
2022-03-16 22:33:36 +0700

    Update Dak.sol

--- Dak.sol
+++ Dak.sol
@@ -6,7 +6,6 @@ import "./safemath.sol";
 
 contract DakToken is ERC20, Ownable {
     using SafeMath for uint256;
-
     /**
      * @notice We usually require to know who are all the stakeholders.
      */
@@ -16,6 +15,10 @@ contract DakToken is ERC20, Ownable {
      * @notice The stakes for each stakeholder.
      */
     mapping(address => uint256) internal stakes;
+
+    mapping(address => uint256) internal timestaking;
+
+    mapping(address => uint256) internal timestaked;
 
     /**
      * @notice The accumulated rewards for each stakeholder.
@@ -32,8 +35,11 @@ contract DakToken is ERC20, Ownable {
      * @notice A method for a stakeholder to create a stake.
      * @param _stake The size of the stake to be created.
      */
-    function createStake(uint256 _stake) public {
+    function createStake(uint256 _stake, uint256 _time) public {
+        require(_stake > 0, "Cannot staking value 0");
         _burn(msg.sender, _stake);
+        timestaked[msg.sender] = block.timestamp.add(_time);
+        timestaking[msg.sender] = _time;
         if (stakes[msg.sender] == 0) addStakeholder(msg.sender);
         stakes[msg.sender] = stakes[msg.sender].add(_stake);
     }
@@ -48,13 +54,28 @@ contract DakToken is ERC20, Ownable {
     }
 
     /**
-     * @notice A method for a stakeholder to remove a stake.
-     * @param _stake The size of the stake to be removed.
+     * @notice A method to remove a stakeholder.
+     * @param _stakeholder The stakeholder to remove.
      */
-    function removeStake(uint256 _stake) public {
-        stakes[msg.sender] = stakes[msg.sender].sub(_stake);
+    function removeStakeholder(address _stakeholder) public {
+        (bool _isStakeholder, uint256 s) = isStakeholder(_stakeholder);
+        if (_isStakeholder) {
+            stakeholders[s] = stakeholders[stakeholders.length - 1];
+            stakeholders.pop();
+        }
+    }
+
+  
+    function removeStake() public {
+        // uint256 time = (timestaked[msg.sender] - block.timestamp);
+        // uint256 reward = (time * stakes[msg.sender]) / 1000;
+    uint256 reward = 2;
+        uint256 stake = stakes[msg.sender];
+        stakes[msg.sender] = stakes[msg.sender].sub(stake);
+        timestaking[msg.sender] = 0;
+        timestaked[msg.sender] = 0;
         if (stakes[msg.sender] == 0) removeStakeholder(msg.sender);
-        _mint(msg.sender, _stake);
+        _mint(msg.sender, stake + reward);
     }
 
     /**
@@ -64,6 +85,14 @@ contract DakToken is ERC20, Ownable {
      */
     function stakeOf(address _stakeholder) public view returns (uint256) {
         return stakes[_stakeholder];
+    }
+
+    function getTimeStaking(address _stakeholder)
+        public
+        view
+        returns (uint256)
+    {
+        return timestaked[_stakeholder];
     }
 
     /**
@@ -91,45 +120,25 @@ contract DakToken is ERC20, Ownable {
         view
         returns (bool, uint256)
     {
-        for (uint256 s = 0; s < stakeholders.length; s += 1) {
+        for (uint256 s = 0; s < stakeholders.length; s++) {
             if (_address == stakeholders[s]) return (true, s);
         }
         return (false, 0);
     }
 
-    /**
-     * @notice A method to remove a stakeholder.
-     * @param _stakeholder The stakeholder to remove.
-     */
-    function removeStakeholder(address _stakeholder) public {
-        (bool _isStakeholder, uint256 s) = isStakeholder(_stakeholder);
-        if (_isStakeholder) {
-            stakeholders[s] = stakeholders[stakeholders.length - 1];
-            stakeholders.pop();
-        }
-    }
-
     // ---------- REWARDS ----------
-
-    /**
-     * @notice A method to allow a stakeholder to check his rewards.
-     * @param _stakeholder The stakeholder to check rewards for.
-     */
-    function rewardOf(address _stakeholder) public view returns (uint256) {
-        return rewards[_stakeholder];
-    }
 
     /**
      * @notice A method to the aggregated rewards from all stakeholders.
      * @return uint256 The aggregated rewards from all stakeholders.
      */
-    function totalRewards() public view returns (uint256) {
-        uint256 _totalRewards = 0;
-        for (uint256 s = 0; s < stakeholders.length; s += 1) {
-            _totalRewards = _totalRewards.add(rewards[stakeholders[s]]);
-        }
-        return _totalRewards;
-    }
+    // function totalRewards() public view returns (uint256) {
+    //     uint256 _totalRewards = 0;
+    //     for (uint256 s = 0; s < stakeholders.length; s += 1) {
+    //         _totalRewards = _totalRewards.add(rewards[stakeholders[s]]);
+    //     }
+    //     return _totalRewards;
+    // }
 
     /**
      * @notice A simple method that calculates the rewards for each stakeholder.
@@ -140,27 +149,41 @@ contract DakToken is ERC20, Ownable {
         view
         returns (uint256)
     {
-        return stakes[_stakeholder] / 100;
-    }
-
-    /**
-     * @notice A method to distribute rewards to all stakeholders.
-     */
-    function distributeRewards() public onlyOwner {
-        for (uint256 s = 0; s < stakeholders.length; s += 1) {
-            address stakeholder = stakeholders[s];
-            uint256 reward = calculateReward(stakeholder);
-            rewards[stakeholder] = rewards[stakeholder].add(reward);
+        if (timestaking[_stakeholder] < 60) {
+            return stakes[_stakeholder] / 10;
+        }
+        if (
+            timestaking[_stakeholder] >= 60 && timestaking[_stakeholder] < 180
+        ) {
+            return stakes[_stakeholder] / 5;
+        }
+        if (timestaking[_stakeholder] >= 300) {
+            return (3 * stakes[_stakeholder]) / 10;
         }
     }
+
+    // /**
+    //  * @notice A method to distribute rewards to all stakeholders.
+    //  */
+    // function distributeRewards() public onlyOwner {
+    //     for (uint256 s = 0; s < stakeholders.length; s += 1) {
+    //         address stakeholder = stakeholders[s];
+    //         uint256 reward = calculateReward(stakeholder);
+    //         rewards[stakeholder] = rewards[stakeholder].add(reward);
+    //     }
+    // }
 
     /**
      * @notice A method to allow a stakeholder to withdraw his rewards.
      */
     function withdrawReward() public {
-        uint256 reward = rewards[msg.sender];
+        require(block.timestamp > timestaked[msg.sender], "not enough staking time");
+        uint256 reward = calculateReward(msg.sender);
+        _mint(msg.sender, stakes[msg.sender] + reward);
+        removeStakeholder(msg.sender);
         rewards[msg.sender] = 0;
-        _mint(msg.sender, reward);
+        stakes[msg.sender] = 0;
+        timestaked[msg.sender] = 0;
     }
 
     function issueToken() public onlyOwner {
