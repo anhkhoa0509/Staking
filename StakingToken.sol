49fc3bfccd1530e52eb43843a9eab1e3431f1bfe
Nguyen Le Anh Khoa <78685355+anhkhoa0509@users.noreply.github.com>
2022-03-25 09:58:35 +0700

    Update StakingToken.sol

--- StakingToken.sol
+++ StakingToken.sol
@@ -255,3 +255,280 @@ contract Staking is Ownable {
 
    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "./safeMath.sol";
+
+contract Staking is Ownable {
+    using SafeMath for uint256;
+  
+    address[][] internal stakeholders;
+    IERC20 private tokenReward;
+
+    // Address owner contract
+    address public admin;
+    mapping(uint256 => mapping(address => uint256)) balances;
+    // Time staking
+    mapping(uint256=>mapping(address=>uint256)) internal startStakingTime;
+    mapping(uint256=>mapping(address=>uint256)) internal endStakingTime;
+    
+
+ 
+    mapping(uint256=> mapping(address => uint256)) internal stakes;
+
+    mapping(uint256=> mapping(address => uint256)) internal rewards;
+    mapping(address=>uint256) internal totalDak;
+
+    
+    mapping(uint256=>mapping(address => uint256)) internal numberPart;
+    
+    struct Staking{
+        address owner;
+        uint256 time;
+        uint interestRate;
+    }
+    Staking[] public Stakings;
+
+
+      constructor (IERC20 token)  {
+        tokenReward = token;
+        admin = msg.sender;
+    }
+    // ---------- STAKES ----------
+
+    
+    function createStaking(uint256 _time, uint _interestRate) public{
+        Stakings.push(
+            Staking(msg.sender,_time,_interestRate)
+        );
+    }
+
+    /**
+     * @notice A method for a stakeholder to create a stake.
+     * @param _stake The size of the stake to be created, 1 _stake = 0.01 ETH
+     *
+     */
+    function createStake(uint256 _stake, uint256 _time,uint256 _id) public payable{
+        uint stake = _stake * 10 ** 16;
+
+        require(_stake  > 0 &&  msg.value > 0 && stake == msg.value, "Cannot staking value 0");
+
+        balances[_id][msg.sender] = msg.value;
+        numberPart[_id][msg.sender] = 1;
+
+        startStakingTime[_id][msg.sender] = block.timestamp;
+
+        endStakingTime[_id][msg.sender] = block.timestamp.add(_time);
+
+        rewards[_id][msg.sender] = calculateReward(stake,Stakings[_id].time,Stakings[_id].interestRate);
+        
+        if (stakes[_id][msg.sender] == 0) addStakeholder(msg.sender,_id);
+
+        stakes[_id][msg.sender] = stakes[_id][msg.sender].add(_stake);
+    }
+
+  
+
+  
+
+    function getBalanceStake(address _userAdress,uint256 _id) public view returns(uint){
+        uint value = balances[_id][_userAdress];
+        // uint time = block.timestamp - depositTime[userAdress];
+        // return value + uint((value*7+time)/(100*365*24*60*62)) +1;
+        return value;
+    }
+
+    function getReward(address _userAdress,uint256 _id) public view returns(uint){
+        return rewards[_id][_userAdress];
+    }
+  
+
+    // function addMoneyToContract() public payable{
+    //     toltalBalances+= msg.value;
+    // }
+
+
+    /**
+     * @notice A method to add a stakeholder.
+     * @param _stakeholder The stakeholder to add.
+     */
+    function addStakeholder(address _stakeholder,uint256 _id) public {
+        (bool _isStakeholder, ) = isStakeholder(_stakeholder,_id);
+        if (!_isStakeholder) stakeholders[_id].push(_stakeholder);
+    }
+
+    /**
+     * @notice A method to remove a stakeholder.
+     * @param _stakeholder The stakeholder to remove.
+     */
+    function removeStakeholder(address _stakeholder,uint256 _id) public {
+        (bool _isStakeholder, uint256 s) = isStakeholder(_stakeholder,_id);
+        if (_isStakeholder) {
+            stakeholders[s] = stakeholders[stakeholders.length - 1];
+            stakeholders.pop();
+        }
+    }
+
+  
+    function removeStake(uint256 _id) public {
+        // uint256 time = (timestaked[msg.sender] - block.timestamp);
+        // uint256 reward = (time * stakes[msg.sender]) / 1000;
+        uint256 reward = 2;
+        uint256 stake = stakes[_id][msg.sender];
+        stakes[_id][msg.sender] = stakes[_id][msg.sender].sub(stake);
+        endStakingTime[_id][msg.sender] = 0;
+      
+        if (stakes[_id][msg.sender] == 0) removeStakeholder(msg.sender,_id);
+        // _mint(msg.sender, stake + reward);
+    }
+
+    /**
+     * @notice A method to retrieve the stake for a stakeholder.
+     * @param _stakeholder The stakeholder to retrieve the stake for.
+     * @return uint256 The amount of wei staked.
+     */
+    function stakeOf(address _stakeholder,uint256 _id) public view returns (uint256) {
+        return stakes[_id][_stakeholder];
+    }
+
+    function getTimeStaking(address _stakeholder,uint256 _id)
+        public
+        view
+        returns (uint256)
+    {
+        return endStakingTime[_id][_stakeholder];
+    }
+
+    /**
+     * @notice A method to the aggregated stakes from all stakeholders.
+     * @return uint256 The aggregated stakes from all stakeholders.
+     */
+    function totalStakes(uint256 _id) public view returns (uint256) {
+        uint256 _totalStakes = 0;
+        for (uint256 s = 0; s < stakeholders[_id].length; s += 1) {
+            _totalStakes = _totalStakes.add(stakes[_id][stakeholders[_id][s]]);
+        }
+        return _totalStakes;
+    }
+
+    // ---------- STAKEHOLDERS ----------
+
+    /**
+     * @notice A method to check if an address is a stakeholder.
+     * @param _address The address to verify.
+     * @return bool, uint256 Whether the address is a stakeholder,
+     * and if so its position in the stakeholders array.
+     */
+    function isStakeholder(address _address,uint256 _id)
+        public
+        view
+        returns (bool, uint256)
+    {
+        for (uint256 s = 0; s < stakeholders[_id].length; s++) {
+            if (_address == stakeholders[_id][s]) return (true, s);
+        }
+        return (false, 0);
+    }
+
+    // ---------- REWARDS ----------
+
+    /**
+     * @notice A method to the aggregated rewards from all stakeholders.
+     * @return uint256 The aggregated rewards from all stakeholders.
+     */
+    // function totalRewards() public view returns (uint256) {
+    //     uint256 _totalRewards = 0;
+    //     for (uint256 s = 0; s < stakeholders.length; s += 1) {
+    //         _totalRewards = _totalRewards.add(rewards[stakeholders[s]]);
+    //     }
+    //     return _totalRewards;
+    // }
+
+    // /**
+    //  * @notice A simple method that calculates the rewards for each stakeholder.
+    //  * @param _stakeholder The stakeholder to calculate rewards for.
+    //  */
+
+    function calculateReward(uint256 _amount,uint256 _time,uint _interestRate) public view returns(uint){
+        if (_amount < 10) {
+            return _interestRate;
+        }
+        if (
+            _amount >= 10 && _amount < 50
+        ) {
+            return _interestRate*2;
+        }
+        else{
+            return _interestRate*3;
+        }
+    }
+
+    // /**
+    //  * @notice A method to distribute rewards to all stakeholders.
+    //  */
+    // function distributeRewards() public onlyOwner {
+    //     for (uint256 s = 0; s < stakeholders.length; s += 1) {
+    //         address stakeholder = stakeholders[s];
+    //         uint256 reward = calculateReward(stakeholder);
+    //         rewards[stakeholder] = rewards[stakeholder].add(reward);
+    //     }
+    // }
+
+    function withdrawRawardPart(uint256 _id) public {
+        uint256 time = numberPart[_id][msg.sender]*(endStakingTime[_id][msg.sender] - startStakingTime[_id][msg.sender])/4;
+        uint256 currentTime = startStakingTime[_id][msg.sender].add(time);
+        require(numberPart[_id][msg.sender] < 4, "the number of times to receive the reward has expired");
+        require(block.timestamp > currentTime, "not enough staking time");
+        numberPart[_id][msg.sender] = numberPart[_id][msg.sender].add(1);
+        
+        tokenReward.transferFrom(admin, msg.sender, rewards[_id][msg.sender]/4);
+
+        totalDak[msg.sender] = totalDak[msg.sender].add(rewards[_id][msg.sender]/4);
+    }
+
+    /**
+     * @notice A method to allow a stakeholder to withdraw his rewards.
+     */
+    function withdrawReward(uint256 _id) public payable{
+        require(block.timestamp > endStakingTime[_id][msg.sender], "not enough staking time");
+        uint amountToTransfer = getBalanceStake(msg.sender,_id);
+        balances[_id][msg.sender] = 0;
+        numberPart[_id][msg.sender] = 4;
+
+        address payable withdrawTo = payable(msg.sender);
+        withdrawTo.transfer(amountToTransfer);
+
+        tokenReward.transferFrom(admin, msg.sender, rewards[_id][msg.sender]/4);
+
+        removeStakeholder(msg.sender,_id);
+        rewards[_id][msg.sender] = 0;
+        stakes[_id][msg.sender] = 0;
+        endStakingTime[_id][msg.sender] = 0;
+    }
+
+   
+}
+
